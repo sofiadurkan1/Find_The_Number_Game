5d7b657aba647cc82195497610149a9ac41a53df
sofia durkan <sofiadurkan@gmail.com>
2021-07-13 20:45:22 -0500

    completed

--- app.js
+++ app.js
@@ -2,44 +2,38 @@
 
 let btn = document.getElementById("submit");
 let restartbtn = document.getElementById("restart");
+var inputUser = document.getElementById("userInput");
 let count = 0;
-
 
 let output = document.getElementById("text");
 
-let number = Math.floor(Math.random()*100);
-console.log(number);
+let number = Math.floor(Math.random() * 100);
+// console.log(number);
 
-btn.addEventListener("click", function(){
-    let input = document.getElementById("userInput").value;
-    if(input == number){
-        output.innerHTML = `You guessed right, your number was ${number}`
-        count++;
-        document.getElementById("count").innerHTML = count;
-        document.getElementById("won").innerHTML = `You won!,Congrats..`;
+btn.addEventListener("click", function () {
+  let input = document.getElementById("userInput").value;
+  if (input == number) {
+    output.innerHTML = `You guessed right, your number was ${number}`;
+    count++;
+    document.getElementById("count").innerHTML = count;
+    document.getElementById("won").innerHTML = `Congratulations! You won!`;
+  } else if (input < number) {
+    output.innerHTML = `You guessed too low!`;
+    count++;
+    document.getElementById("count").innerHTML = count;
+  } else if (input > number) {
+    output.innerHTML = `You guessed too high!`;
+    count++;
+    document.getElementById("count").innerHTML = count;
+  }
+});
 
-    }else if(input<number) {
-        output.innerHTML = `You guessed too low!`
-        count ++;
-        document.getElementById("count").innerHTML = count;
-
-
-
-    }
-    else if(input>number){
-        output.innerHTML = `You guessed too high!`
-        count ++
-        document.getElementById("count").innerHTML = count;
-
-
-
-    }
-
-
-
-})
-
-restartbtn.addEventListener("click", function(){
-   location.reload()
-})
-
+restartbtn.addEventListener("click", function () {
+  location.reload();
+});
+inputUser.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("submit").click();
+  }
+});
